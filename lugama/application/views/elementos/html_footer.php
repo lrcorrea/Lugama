e4ff1ce72ba5045ad3a92a778057f9fe20c86582
lucaswitcode <lucasrodriguescorrea2006@hotmail.com>
2016-02-18 17:25:22 -0200

    18022016

---
+++ lugama/application/views/elementos/html_footer.php
@@ -0,0 +1,22 @@
+<footer>
+    <div class="centro">
+        <div class="inf_footer">
+            <p>José Rossini, 370</p>
+            <div class="quadrado"></div>
+            <p>Bento Gonçalves - RS</p>
+            <div class="quadrado"></div>
+            <p>(54) 3453.3140</p>
+            <div class="quadrado"></div>
+            <p>pousada@multilugama.com.br</p>
+            <img src="<?= base_url()?>style/imagens/logo_rodape.png" class="logo_ezoom">
+        </div>
+    </div>
+</footer>
+
+<script src="<?= base_url()?>js/jquery.js"></script>
+<script src="<?= base_url()?>js/jquery.devrama.slider.js"></script>
+<script src="<?= base_url()?>style/lightbox/js/lightbox.js"></script>
+<script src="<?= base_url()?>style/slider/js/easySlider1.7.js"></script>
+<script src="<?= base_url()?>js/js.js"></script>
+</body>
+</html>
---
+++ lugama/application/views/home.php
@@ -0,0 +1,112 @@
+
+<div id="modal">
+    <div class="centro">
+        <div class="modal_box">
+            <div id="container">
+                <div id="content">
+                    <div id="slider">
+                        <ul>				
+                            <li>
+                                <img src="<?= base_url()?>style/imagens/cozinha.jpg">
+                                <div class="descricao"><p>Apartamento Luxo 1</p></div>
+                            </li>
+                            <li>
+                                <img src="<?= base_url()?>style/imagens/cozinha.jpg">
+                                <div class="descricao"><p>Apartamento Luxo 2</p></div>
+                            </li>
+                            <li>
+                                <img src="<?= base_url()?>style/imagens/cozinha.jpg">
+                                <div class="descricao"><p>Apartamento Luxo 3</p></div>
+                            </li>
+                            <li>
+                                <img src="<?= base_url()?>style/imagens/cozinha.jpg">
+                                <div class="descricao"><p>Apartamento Luxo 4</p></div>
+                            </li>	
+                        </ul>
+                    </div>
+                </div>
+
+            </div>  
+            <div class="fechar">X</div>
+        </div>
+    </div>
+</div>
+
+<div class="slider_home">
+    <div id="slider_topo">
+        <div data-lazy-background="<?= base_url()?>style/imagens/slider1.jpg">
+            <div data-pos="['22%', '0%']" data-duration="1000" data-effect="fadein" class="bg_descri">
+                <h2>Conheça nossos quartos compartilhados</h2>
+            </div>
+        </div>
+        <div data-lazy-background="<?= base_url()?>style/imagens/slider1.jpg">
+            <div data-pos="['22%', '0%']" data-duration="1000" data-effect="fadein" class="bg_descri">
+                <h2>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras elementum.</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+<section class="apresentacao">
+    <div class="centro">
+        <div class="apresentacao_esq">
+            <h1><span>Seja</span><br/>Bem Vindo!</h1>
+            <p>Situada em uma hospitaleira cidade da Serra Gaúcha, 
+                a Pousada Lugama aposta na total satisfação de seus hóspedes.  
+                A Pousada oferece opções especiais para cada visitante. São 
+                apartamentos conjugados, que se ajustam de acordo com a necessidade 
+                de seus clientes.
+            </p>
+            
+            <a href="#" class="botao_conheca">Conheça Mais</a>
+            
+        </div>
+        <div class="apresentacao_dir">
+            <div class="img_esq"></div>
+            <div class="img_dir"></div>
+            <!--<div class="bg_fixed img_esq"></div>
+            <div class="bg_fixed img_dir"></div>-->
+        </div>    
+    </div>
+</section>
+<section class="acomodacoes">
+    <div class="bg_fixed acomod">
+        <div class="corte_bg"></div>    
+    </div> 
+</section>
+<div class="centro">
+   <div class="titulo_acomodacoes">
+         <h3>ACOMODAÇÕES</h3>
+   </div>
+   <div class="txt_acomodacoes">
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. </p>
+        <a href="#" class="conheca_mais">Conheça Mais</a>
+   </div>
+</div>
+
+<section class="slide_rodape">
+    <div id="content2">
+        <div id="container2">
+            <div class="centro_slider">
+                <div id="slider2">   
+        
+                    <ul>	
+                        <li>
+                            <a class="link_rel" data-lightbox="image-1" href="<?= base_url()?>style/imagens/cozinha.jpg"><img src="<?= base_url()?>style/imagens/cozinha.jpg" alt=""><div class="div_efeito"><p>Apartamento Luxo 01</p></div></a>
+                            <a class="link_rel" data-lightbox="image-2" href="<?= base_url()?>style/imagens/cozinha.jpg"><img src="<?= base_url()?>style/imagens/cozinha.jpg" alt=""><div class="div_efeito"><p>Apartamento Luxo 02</p></div></a>
+                            <a class="link_rel" data-lightbox="image-3" href="<?= base_url()?>style/imagens/cozinha.jpg"><img src="<?= base_url()?>style/imagens/cozinha.jpg" alt=""><div class="div_efeito"><p>Apartamento Luxo 03</p></div></a>
+                            <a class="link_rel" data-lightbox="image-4" href="<?= base_url()?>style/imagens/cozinha.jpg"><img src="<?= base_url()?>style/imagens/cozinha.jpg" alt=""><div class="div_efeito"><p>Apartamento Luxo 04</p></div></a>
+                        </li>
+                        <li>
+                            <a class="link_rel" data-lightbox="image-5" href="<?= base_url()?>style/imagens/cozinha.jpg"><img src="<?= base_url()?>style/imagens/cozinha.jpg" alt=""><div class="div_efeito"><p>Apartamento Luxo 05</p></div></a>
+                            <a class="link_rel" data-lightbox="image-6" href="<?= base_url()?>style/imagens/cozinha.jpg"><img src="<?= base_url()?>style/imagens/cozinha.jpg" alt=""><div class="div_efeito"><p>Apartamento Luxo 06</p></div></a>
+                            <a class="link_rel" data-lightbox="image-7" href="<?= base_url()?>style/imagens/cozinha.jpg"><img src="<?= base_url()?>style/imagens/cozinha.jpg" alt=""><div class="div_efeito"><p>Apartamento Luxo 07</p></div></a>
+                            <a class="link_rel" data-lightbox="image-8" href="<?= base_url()?>style/imagens/cozinha.jpg"><img src="<?= base_url()?>style/imagens/cozinha.jpg" alt=""><div class="div_efeito"><p>Apartamento Luxo 08</p></div></a>
+                        </li>
+                    </ul>        
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
